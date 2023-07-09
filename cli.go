442061ac22409c2e445eaa2eb9a3537ce9417488
CodapeWild <codape.wild@gmail.com>
2023-07-09 15:39:42 +0800

    fix

--- cli.go
+++ cli.go
@@ -26,8 +26,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "opentracing-agent",
-	Short: "A brief description of your application",
+	Use:     "opentracing-agent",
+	Aliases: []string{"opagt"},
+	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
@@ -54,6 +55,35 @@ to quickly create a Cobra application.`,
 	},
 }
 
+// reloadCmd represents the reload command
+var reloadCmd = &cobra.Command{
+	Use:   "reload",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("reload called")
+	},
+}
+
+var remapCmd = &cobra.Command{
+	Use:   "remap",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("remap called")
+	},
+}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -74,4 +104,6 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.AddCommand(configCmd)
+	rootCmd.AddCommand(reloadCmd)
+	rootCmd.AddCommand(remapCmd)
 }
